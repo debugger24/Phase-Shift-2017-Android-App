95066db1e2c0ea1f1e82b5b755477e02d78cfcbd
Rahul Kumar <rahulcomp24@gmail.com>
2017-06-06 17:51:35 +0530

    Move Newsfeed to Newsfeed Package

--- PhaseShift2017/app/src/main/java/me/rahulk/phaseshift2017/Newsfeed/NewsfeedFragment.java
+++ PhaseShift2017/app/src/main/java/me/rahulk/phaseshift2017/Newsfeed/NewsfeedFragment.java
@@ -1,4 +1,4 @@
-package me.rahulk.phaseshift2017;
+package me.rahulk.phaseshift2017.Newsfeed;
 
 import android.content.Context;
 import android.net.Uri;
@@ -7,6 +7,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import me.rahulk.phaseshift2017.R;
 
 
 /**
