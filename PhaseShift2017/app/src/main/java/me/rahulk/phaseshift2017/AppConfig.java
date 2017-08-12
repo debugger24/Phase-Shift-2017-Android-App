f49292ae0fbd1afb09e814a48510954752de0e0a
Rahul Kumar <rahulcomp24@gmail.com>
2017-08-12 20:28:59 +0530

    Add AppConfig

---
+++ PhaseShift2017/app/src/main/java/me/rahulk/phaseshift2017/AppConfig.java
@@ -0,0 +1,9 @@
+package me.rahulk.phaseshift2017;
+
+/**
+ * Created by debugger24 on 12/08/17.
+ */
+
+public class AppConfig {
+    public static String URL_EVENTS = "http://192.168.100.113/~debugger24/ps/events.php";
+}
