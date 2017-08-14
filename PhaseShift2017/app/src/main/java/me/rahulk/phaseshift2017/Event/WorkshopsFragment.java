aae8f823a8a1c3bd7c854bca5b8e7527a67a0e50
Rahul Kumar <rahulcomp24@gmail.com>
2017-08-14 22:16:09 +0530

    Implement WorkshopFragment

--- PhaseShift2017/app/src/main/java/me/rahulk/phaseshift2017/Event/WorkshopsFragment.java
+++ PhaseShift2017/app/src/main/java/me/rahulk/phaseshift2017/Event/WorkshopsFragment.java
@@ -1,13 +1,20 @@
 package me.rahulk.phaseshift2017.Event;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.support.v4.app.LoaderManager;
+import android.widget.ListView;
 
+import me.rahulk.phaseshift2017.Data.PhaseShiftContract;
 import me.rahulk.phaseshift2017.R;
 
 
@@ -19,7 +26,7 @@ import me.rahulk.phaseshift2017.R;
  * Use the {@link WorkshopsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class WorkshopsFragment extends Fragment {
+public class WorkshopsFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -30,6 +37,23 @@ public class WorkshopsFragment extends Fragment {
     private String mParam2;
 
     private OnFragmentInteractionListener mListener;
+
+    private EventCursorAdapter eventCursorAdapter;
+    private static final int FORECAST_LOADER = 1;
+
+    private static final String[] EVENT_COLUMNS = {
+            PhaseShiftContract.EventEntry._ID,
+            PhaseShiftContract.EventEntry.COLUMNS_EVENT_TITLE,
+            PhaseShiftContract.EventEntry.COLUMNS_EVENT_DEPARTMENT,
+            PhaseShiftContract.EventEntry.COLUMNS_EVENT_BMSCE,
+            PhaseShiftContract.EventEntry.COLUMNS_EVENT_FULL,
+    };
+
+    static final int COL_EVENT_ID = 0;
+    static final int COL_EVENT_TITLE = 1;
+    static final int COL_EVENT_DEPARTMENT = 2;
+    static final int COL_EVENT_BMSCE = 3;
+    static final int COL_EVENT_FULL = 4;
 
     public WorkshopsFragment() {
         // Required empty public constructor
@@ -66,7 +90,14 @@ public class WorkshopsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_workshops, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_workshops, container, false);
+        ListView listView = (ListView) rootView.findViewById(R.id.lstEvent);
+
+        eventCursorAdapter = new EventCursorAdapter(getActivity(), null, 0);
+
+        listView.setAdapter(eventCursorAdapter);
+
+        return rootView;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -88,9 +119,34 @@ public class WorkshopsFragment extends Fragment {
     }
 
     @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        getLoaderManager().initLoader(1, null, this);
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        String sortOrder = PhaseShiftContract.EventEntry.COLUMNS_EVENT_DEPARTMENT + " ASC";
+        Uri allEvents = PhaseShiftContract.EventEntry.buildEventUri();
+        String selection = PhaseShiftContract.EventEntry.COLUMNS_EVENT_TYPE + " = ?";
+        String[] selectionArgs = {"Workshop"};
+        return new CursorLoader(getActivity(), allEvents, EVENT_COLUMNS, selection, selectionArgs, sortOrder);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        eventCursorAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        eventCursorAdapter.swapCursor(null);
     }
 
     /**
