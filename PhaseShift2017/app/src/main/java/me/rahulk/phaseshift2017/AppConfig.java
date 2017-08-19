720d42b32140c1c26a9a92e0521748cdb156ce2b
Rahul Kumar <rahulcomp24@gmail.com>
2017-08-19 09:12:05 +0530

    Add Server Links

--- PhaseShift2017/app/src/main/java/me/rahulk/phaseshift2017/AppConfig.java
+++ PhaseShift2017/app/src/main/java/me/rahulk/phaseshift2017/AppConfig.java
@@ -5,8 +5,8 @@ package me.rahulk.phaseshift2017;
  */
 
 public class AppConfig {
-    public static String HOST_NAME = "http://192.168.43.72/";
+    public static String HOST_NAME = "http://www.rahulk.me/";
 
-    public static String URL_NEWSFEEDS = HOST_NAME + "~debugger24/ps/feed.php";
-    public static String URL_EVENTS = HOST_NAME + "~debugger24/ps/events.php";
+    public static String URL_NEWSFEEDS = HOST_NAME + "ps2017/feed.php";
+    public static String URL_EVENTS = HOST_NAME + "ps2017/events.php";
 }
