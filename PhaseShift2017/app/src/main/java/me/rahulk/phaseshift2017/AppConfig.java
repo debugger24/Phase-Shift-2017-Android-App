253ee13cc9fbf4724cb7fe480ca05461c25c8335
Rahul Kumar <rahulcomp24@gmail.com>
2017-08-23 19:23:43 +0530

    Update API URLs

--- PhaseShift2017/app/src/main/java/me/rahulk/phaseshift2017/AppConfig.java
+++ PhaseShift2017/app/src/main/java/me/rahulk/phaseshift2017/AppConfig.java
@@ -7,7 +7,7 @@ package me.rahulk.phaseshift2017;
 public class AppConfig {
     public static String HOST_NAME = "http://www.rahulk.me/";
 
-    public static String URL_SCHEDULE = HOST_NAME + "ps2017/day1.json";
+    public static String URL_SCHEDULE = HOST_NAME + "ps2017/schedule.json";
     public static String URL_NEWSFEEDS = HOST_NAME + "ps2017/feed.php";
     public static String URL_EVENTS = HOST_NAME + "ps2017/events.php";
 }
