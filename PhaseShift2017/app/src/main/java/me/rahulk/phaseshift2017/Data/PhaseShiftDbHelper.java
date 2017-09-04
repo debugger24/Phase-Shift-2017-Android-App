15a6c9d897aff600524962912d70a1c55859a18d
Rahul Kumar <rahulcomp24@gmail.com>
2017-09-04 09:10:49 +0530

    Update Database Version to 10

--- PhaseShift2017/app/src/main/java/me/rahulk/phaseshift2017/Data/PhaseShiftDbHelper.java
+++ PhaseShift2017/app/src/main/java/me/rahulk/phaseshift2017/Data/PhaseShiftDbHelper.java
@@ -13,7 +13,7 @@ public class PhaseShiftDbHelper extends SQLiteOpenHelper {
 
     public static final String LOG_TAG = PhaseShiftDbHelper.class.getSimpleName();
     private static final String DATABASE_NAME = "phaseshift.db";
-    private static final int DATABASE_VERSION = 9;
+    private static final int DATABASE_VERSION = 10;
 
     public PhaseShiftDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
