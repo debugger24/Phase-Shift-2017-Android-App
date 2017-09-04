c101c909ecded8f9e6a72cce72b125d22d22dafe
Rahul Kumar <rahulcomp24@gmail.com>
2017-09-04 08:23:07 +0530

    Implement Sort and Filter in Admin Stats

--- PhaseShift2017/app/src/main/java/me/rahulk/phaseshift2017/Admin/Stats.java
+++ PhaseShift2017/app/src/main/java/me/rahulk/phaseshift2017/Admin/Stats.java
@@ -14,11 +14,14 @@ import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.EditText;
@@ -39,9 +42,11 @@ public class Stats extends AppCompatActivity implements LoaderManager.LoaderCall
     SearchView searchView;
     String search = "";
     String department = "";
-
+    String order = PhaseShiftContract.EventEntry.COLUMNS_EVENT_TITLE + " ASC ";
     private EventsCursorAdapter eventsCursorAdapter;
     private static final int FORECAST_LOADER = 1;
+    private Toolbar toolbar;
+    ArrayList<String> selectionArgs = new ArrayList<String>();
 
     private static final String[] EVENT_COLUMNS = {
             PhaseShiftContract.EventEntry._ID,
@@ -60,9 +65,98 @@ public class Stats extends AppCompatActivity implements LoaderManager.LoaderCall
     static final int COL_EVENT_ID = 0;
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.event_stats, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.editSearch:
+                // Do Nothing Here
+                break;
+            case R.id.menuSortTitle:
+                order = PhaseShiftContract.EventEntry.COLUMNS_EVENT_TITLE + " ASC ";
+                break;
+            case R.id.menuSortDepartment:
+                order = PhaseShiftContract.EventEntry.COLUMNS_EVENT_DEPARTMENT + " ASC ";
+                break;
+            case R.id.menuFilterAll:
+                department = "";
+                break;
+            case R.id.menuFilterBT:
+                department = "Bio-Technology";
+                break;
+            case R.id.menuFilterCV:
+                department = "Civil Engineering";
+                break;
+            case R.id.menuFilterCH:
+                department = "Chemical Engineering";
+                break;
+            case R.id.menuFilterCS:
+                department = "Computer Science and Engineering";
+                break;
+            case R.id.menuFilterEE:
+                department = "Electrical and Electronics Engineering";
+                break;
+            case R.id.menuFilterEC:
+                department = "Electronics and Communication Engineering";
+                break;
+            case R.id.menuFilterEIE:
+                department = "Electronics and Instrumentation Engineering";
+                break;
+            case R.id.menuFilterIEM:
+                department = "Industrial Engineering and Management";
+                break;
+            case R.id.menuFilterIS:
+                department = "Information Science and Engineering";
+                break;
+            case R.id.menuFilterMCA:
+                department = "Master of Computer Application";
+                break;
+            case R.id.menuFilterME:
+                department = "Mechanical Engineering";
+                break;
+            case R.id.menuFilterML:
+                department = "Medical Electronics";
+                break;
+            case R.id.menuFilterTC:
+                department = "Telecommunication Engineering";
+                break;
+            case R.id.menuFilterIEEE:
+                department = "BMSCE IEEE";
+                break;
+            case R.id.menuFilterARC:
+                department = "Architecture";
+                break;
+            case R.id.menuFilterMBA:
+                department = "Master of Business Administration";
+                break;
+            case R.id.menuFilterPENT:
+                department = "Pentagram";
+                break;
+            case R.id.menuFilterAU:
+                department = "Alternate Universe";
+                break;
+            case R.id.menuFilterQUIZ:
+                department = "Qcaine - Quiz club";
+                break;
+        }
+        getSupportLoaderManager().restartLoader(1, null, Stats.this);
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stats);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
 
         sharedPreferences = this.getSharedPreferences("PhaseShift2017", this.MODE_PRIVATE);
 
@@ -124,16 +218,20 @@ public class Stats extends AppCompatActivity implements LoaderManager.LoaderCall
 
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        String sortOrder = PhaseShiftContract.EventEntry.COLUMNS_EVENT_ACTIVE + " DESC, " +
-                PhaseShiftContract.EventEntry.COLUMNS_EVENT_FULL + " ASC, " +
-                PhaseShiftContract.EventEntry.COLUMNS_EVENT_TITLE + " ASC";
+        String sortOrder = order;
         String selection = PhaseShiftContract.EventEntry.COLUMNS_EVENT_TITLE + " LIKE ? ";
-        ArrayList<String> selectionArgs = new ArrayList<String>();
+        selectionArgs.clear();
         selectionArgs.add("%" + search + "%");
 
+        // Department
         if (!department.equals("")) {
             selection += " AND " + PhaseShiftContract.EventEntry.COLUMNS_EVENT_DEPARTMENT + " LIKE ?";
-            selectionArgs.add("%" + getAccessCode().substring(12) + "%");
+
+            if (getAccessCode().substring(0, 4).equals("DEPT")) {
+                selectionArgs.add("%" + getAccessCode().substring(12) + "%");
+            } else {
+                selectionArgs.add("%" + department + "%");
+            }
         }
 
         Uri allEvents = PhaseShiftContract.EventEntry.buildEventUri();
