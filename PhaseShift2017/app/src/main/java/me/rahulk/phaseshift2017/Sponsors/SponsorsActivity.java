f7e9aef3b822f6b80d09f18232df10dcc7c4d532
Rahul Kumar <rahulcomp24@gmail.com>
2017-09-13 03:48:35 +0530

    Implement Sponsors Activity

--- PhaseShift2017/app/src/main/java/me/rahulk/phaseshift2017/Sponsors/SponsorsActivity.java
+++ PhaseShift2017/app/src/main/java/me/rahulk/phaseshift2017/Sponsors/SponsorsActivity.java
@@ -1,15 +1,143 @@
 package me.rahulk.phaseshift2017.Sponsors;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import com.android.volley.Cache;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+import me.rahulk.phaseshift2017.AppController;
+import me.rahulk.phaseshift2017.Newsfeed.FeedItem;
+import me.rahulk.phaseshift2017.Newsfeed.FeedListAdapter;
 import me.rahulk.phaseshift2017.R;
 
+import static me.rahulk.phaseshift2017.AppConfig.URL_NEWSFEEDS;
+import static me.rahulk.phaseshift2017.AppConfig.URL_SPONSORS;
+
 public class SponsorsActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private List<SponsorRow> sponsorRows;
+    private SponsorsListAdapter sponsorsListAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sponsors);
+
+        listView = (ListView) findViewById(R.id.sponsorsList);
+        sponsorRows = new ArrayList<SponsorRow>();
+
+        sponsorsListAdapter = new SponsorsListAdapter(SponsorsActivity.this, sponsorRows);
+        listView.setAdapter(sponsorsListAdapter);
+
+        refreshData();
+    }
+
+    private void updateSponsors() {
+        Cache cache = AppController.getInstance().getRequestQueue().getCache();
+        Cache.Entry entry = cache.get(URL_SPONSORS);
+
+        if (entry != null) {
+            // fetch the data from cache
+            try {
+                String data = new String(entry.data, "UTF-8");
+                try {
+                    parseJsonFeed(new JSONObject(data));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        } else {
+            // making fresh volley request and getting json
+            if (isNetworkAvailable()) {
+                refreshData();
+            } else {
+                Toast.makeText(getApplicationContext(), "No Internet Connection", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+
+    }
+
+    private void refreshData() {
+        JsonObjectRequest jsonReq = new JsonObjectRequest(Request.Method.GET, URL_SPONSORS, null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                VolleyLog.d("SPONSOR_TAG", "Response: " + response.toString());
+                if (response != null) {
+                    parseJsonFeed(response);
+                }
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d("SPONSOR_TAG", "Error: " + error.getMessage());
+//                swipeContainer.setRefreshing(false);
+            }
+        });
+
+        // Adding request to volley request queue
+        AppController.getInstance().addToRequestQueue(jsonReq);
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    private void parseJsonFeed(JSONObject response) {
+        Log.v("parseJsonFeed", response.toString());
+        try {
+            JSONArray sponsorRowArray = response.getJSONArray("sponsors");
+            sponsorRows.clear();
+            for (int i = 0; i < sponsorRowArray.length(); i++) {
+
+                JSONObject sponsorRowObject = (JSONObject) sponsorRowArray.get(i);
+
+                SponsorRow sponsorRow = new SponsorRow(sponsorRowObject.getString("type"));
+
+                JSONArray sponsorsArray = sponsorRowObject.getJSONArray("sponsors");
+                for (int j = 0; j < sponsorsArray.length(); j++) {
+                    JSONObject sponsorObject = (JSONObject) sponsorsArray.get(j);
+                    Sponsor sponsor = new Sponsor(sponsorObject.getString("name"), sponsorObject.getString("logo"), sponsorObject.getString("url"));
+                    sponsorRow.addSponsor(sponsor);
+                }
+                sponsorRows.add(sponsorRow);
+            }
+
+            Log.v("SPONSOR ROWS", sponsorRows.toString());
+
+            // notify data changes to list adapater
+            sponsorsListAdapter = new SponsorsListAdapter(SponsorsActivity.this, sponsorRows);
+            listView.setAdapter(sponsorsListAdapter);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        } finally {
+//            swipeContainer.setRefreshing(false);
+        }
     }
 }
