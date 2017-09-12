eb068139fc1d4388eb03c236966c3ff99471da14
Rahul Kumar <rahulcomp24@gmail.com>
2017-09-12 14:23:54 +0530

    Add Blank Sponsors Activity

---
+++ PhaseShift2017/app/src/main/java/me/rahulk/phaseshift2017/Sponsors/SponsorsActivity.java
@@ -0,0 +1,15 @@
+package me.rahulk.phaseshift2017.Sponsors;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import me.rahulk.phaseshift2017.R;
+
+public class SponsorsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sponsors);
+    }
+}
