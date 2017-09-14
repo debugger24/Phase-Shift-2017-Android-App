0a858f6875a363196993035d5e39099e07ac10b7
Rahul Kumar <rahulcomp24@gmail.com>
2017-09-15 01:51:25 +0530

    Build GlideApp

---
+++ PhaseShift2017/app/src/main/java/me/rahulk/phaseshift2017/Sponsors/MyAppGlideModule.java
@@ -0,0 +1,11 @@
+package me.rahulk.phaseshift2017.Sponsors;
+
+/**
+ * Created by debugger24 on 15/09/17.
+ */
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public final class MyAppGlideModule extends AppGlideModule {}
